97c5d5c8978d1bac0c1d6b7992af816a8aa88860
Myunghwan Kang <myukang@c10r4s13.42seoul.kr>
2022-05-15 22:14:28 +0900

    p

---
+++ philo/philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myukang <myukang@student.42.kr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/15 22:01:35 by myukang           #+#    #+#             */
+/*   Updated: 2022/05/15 22:06:03 by myukang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+ * stdio.h for : printf
+ * stdlib.h for : malloc, free
+ * unistd.h for : write, usleeep
+ * string.h for : memset
+ * sys/time.h for : gettimeofday
+ * pthread.h for : pthread functions..
+ * */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
